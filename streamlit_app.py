b8ece3291660fe1ff97d7b4ca4e614d3684b790a
llyasamy <shallyardhany1@gmail.com>
2026-05-12 08:42:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 kimia")
+st.title("🎈 kimia ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
